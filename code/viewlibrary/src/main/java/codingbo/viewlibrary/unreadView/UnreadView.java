6707a1b0353ff091c7225be3d47a221b4ed38c19
liangbo <liangbo@qiaxz.com>
2017-09-28 18:39:11 +0800

    增加第二个方式 根据圆外点切线方式(还在研究)

--- code/viewlibrary/src/main/java/codingbo/viewlibrary/unreadView/UnreadView.java
+++ code/viewlibrary/src/main/java/codingbo/viewlibrary/unreadView/UnreadView.java
@@ -16,6 +16,9 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 仿QQ未读小红点
  */
@@ -45,6 +48,7 @@ public class UnreadView extends View {
     private int textSize = 20;
     private Path mPath;
     private int defaultRadius;
+    private Paint mPointPaint;
 
 
     public UnreadView(Context context) {
@@ -72,6 +76,10 @@ public class UnreadView extends View {
 
         mPath = new Path();
 
+        mPointPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPointPaint.setStrokeWidth(10);
+        mPointPaint.setStrokeCap(Paint.Cap.ROUND);
+
 
     }
 
@@ -86,7 +94,7 @@ public class UnreadView extends View {
         mCRadius = defaultRadius;
 
         mFX = mCX + 300;
-        mFY = mCY + 300;
+        mFY = mCY - 300;
         mFRadius = mCRadius;
     }
 
@@ -96,12 +104,13 @@ public class UnreadView extends View {
 
         float dY = mFY - mCY;
         float dX = mFX - mCX;
-        mDistance = Math.sqrt(Math.hypot(dX, dY));
+        mDistance = Math.hypot(dX, dY);
 
-        mCRadius = (int) Math.abs(defaultRadius - mDistance);
+        mCRadius = (int) (defaultRadius - mDistance / 10);
+        if (mCRadius <= 0) {
+            mCRadius = 1;
+        }
 
-        Log.d(TAG, "mDistance: " + mDistance);
-        Log.d(TAG, "mCRadius: " + mCRadius);
 
         mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(1);
@@ -111,7 +120,46 @@ public class UnreadView extends View {
         //draw finger circle
         canvas.drawCircle(mFX, mFY, mFRadius, mPaint);
 
+        float cX = mCX / 2 + mFX / 2;
+        float cY = mCY / 2 + mFY / 2;
 
+        mode1(canvas, dX, dY, cX, cY);
+//        mode2(canvas, cX, cY);
+
+
+    }
+
+    private void mode2(Canvas canvas, float cX, float cY) {
+        //        List<PointF> cPointList = getPointOfContact(new PointF(mCX, mCY), mCRadius, new PointF(mFX, mFY));
+        List<PointF> fPointList = getPointOfContact(new PointF(mFX, mFY), mFRadius, new PointF(mCX, mCY));
+
+        canvas.drawPoint(cX, cY, mPointPaint);
+        if (fPointList != null) {
+            for (PointF p : fPointList) {
+                canvas.drawPoint(p.x, p.y, mPointPaint);
+            }
+        }
+
+
+//        if (cPointList != null) {
+//            for (PointF p : cPointList) {
+//                canvas.drawPoint(p.x, p.y, mPointPaint);
+//            }
+//        }
+
+//        if (cPointList != null && cPointList.size() == 2
+//                && fPointList != null && fPointList.size() == 2) {
+//
+//            PointF c1 = cPointList.get(0);
+//            PointF c2 = cPointList.get(1);
+//            PointF f1 = fPointList.get(0);
+//            PointF f2 = fPointList.get(1);
+//
+//            drawAdhesion(canvas, new PointF(cX, cY), c1, c2, f2, f1);
+//        }
+    }
+
+    private void mode1(Canvas canvas, float dX, float dY, float cX, float cY) {
         double angle;
         if (dX == 0) {
             angle = 0;
@@ -131,19 +179,31 @@ public class UnreadView extends View {
         float f2x = (float) (mFX + Math.sin(angle) * mFRadius);
         float f2y = (float) (mFY + Math.cos(angle) * mFRadius);
 
-        float cX = mCX / 2 + mFX / 2;
-        float cY = mCY / 2 + mFY / 2;
 
+        drawAdhesion(canvas, new PointF(cX, cY),
+                new PointF(c1x, c1y),
+                new PointF(c2x, c2y),
+                new PointF(f1x, f1y),
+                new PointF(f2x, f2y));
+    }
+
+    private void drawAdhesion(Canvas canvas, PointF c, PointF c1, PointF c2, PointF f1, PointF f2) {
         mPaint.setColor(Color.RED);
 //        mPaint.setStyle(Paint.Style.STROKE);
-
         mPath.reset();
-        mPath.moveTo(c1x, c1y);
-        mPath.cubicTo(c1x, c1y, cX, cY, f1x, f1y);
-        mPath.lineTo(f2x, f2y);
-        mPath.cubicTo(f2x, f2y, cX, cY, c2x, c2y);
+        mPath.moveTo(c1.x, c1.y);
+        mPath.cubicTo(c1.x, c1.y, c.x, c.y, f1.x, f1.y);
+        mPath.lineTo(f2.x, f2.y);
+        mPath.cubicTo(f2.x, f2.y, c.x, c.y, c2.x, c2.y);
         mPath.close();
         canvas.drawPath(mPath, mPaint);
+
+
+//        canvas.drawPoint(c.x, c.y, mPointPaint);
+//        canvas.drawPoint(c1.x, c1.y, mPointPaint);
+//        canvas.drawPoint(c2.x, c2.y, mPointPaint);
+//        canvas.drawPoint(f1.x, f1.y, mPointPaint);
+//        canvas.drawPoint(f2.x, f2.y, mPointPaint);
     }
 
     @Override
@@ -161,9 +221,9 @@ public class UnreadView extends View {
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
-//                mFX = mCX;
-//                mFY = mCY;
 
+//                mFX = x;
+//                mFY = y;
                 ObjectAnimator animator = ObjectAnimator.ofObject(this, "point", new PointFEvaluator(),
                         new PointF(x, y), new PointF(mCX, mCY));
                 animator.setDuration(300);
@@ -185,5 +245,106 @@ public class UnreadView extends View {
 
     public PointF getPoint() {
         return new PointF(mFX, mFY);
+    }
+
+
+    /**
+     * 根据圆外一点 求与圆的两个切点
+     *
+     * @param circlePoint 圆心
+     * @param radius      半径
+     * @param outPoint    圆外一点
+     * @return 切点  2个切点 在圆外 1个切点在圆上  null为不存在(圆内)
+     */
+    public List<PointF> getPointOfContact(PointF circlePoint, float radius, PointF outPoint) {
+        if (circlePoint == null || outPoint == null || radius <= 0) {
+            return null;
+        }
+
+        List<PointF> result = new ArrayList<>();
+
+        float dY = outPoint.y - circlePoint.y;
+        float dX = outPoint.x - circlePoint.y;
+        double distance = Math.hypot(dX, dY);
+        if (radius > distance) {
+            return result;
+        } else if (radius == distance) {
+            //在圆上
+            result.add(outPoint);
+            return result;
+        }
+
+
+        PointF p0 = new PointF();
+        PointF p1 = new PointF();
+
+        // 两点的斜率
+
+        double k = dY / dX;
+
+        /*
+         * 象限
+         */
+        int quadrant = getQuadrant(circlePoint, outPoint);
+        /*
+            两个角度
+
+            切线 圆心 连线夹角 a1  (0, 90)
+
+            连线 和水平的夹角 a2
+
+            计算偏移量
+
+            a1 - a2
+            a1 + a2
+
+         */
+
+        double a1 = Math.acos(radius / distance);
+        double a2 = Math.atan(k);
+
+        switch (quadrant) {
+            case 1:
+                Log.d(TAG, "quadrant: " + 1);
+                break;
+            case 2:
+                a2 += Math.PI;
+                Log.d(TAG, "quadrant: " + 2);
+                break;
+            case 3:
+                a2 += Math.PI;
+                Log.d(TAG, "quadrant: " + 3);
+                break;
+            case 4:
+                a2 += Math.PI * 2;
+                Log.d(TAG, "quadrant: " + 4);
+                break;
+        }
+
+        p0.x = (float) (circlePoint.x + Math.cos(a2 - a1) * radius);
+        p0.y = (float) (circlePoint.y + Math.sin(a2 - a1) * radius);
+        result.add(p0);
+
+        p1.x = (float) (circlePoint.x - Math.cos((a2 + a1) + Math.PI) * radius);
+        p1.y = (float) (circlePoint.y - Math.sin((a2 + a1) + Math.PI) * radius);
+        result.add(p1);
+
+        Log.d(TAG, "a1 : " + (a1 / Math.PI * 180));
+        Log.d(TAG, "a2 : " + (a2 / Math.PI * 180));
+
+        return result;
+    }
+
+    private int getQuadrant(PointF cp, PointF op) {
+        if (op.x >= cp.x && op.y >= cp.y) {
+            return 1;
+        } else if (op.x < cp.x && op.y >= cp.y) {
+            return 2;
+        } else if (op.x < cp.x && op.y < cp.y) {
+            return 3;
+        } else if (op.x >= cp.x && op.y < cp.y) {
+            return 4;
+        }
+        return 1;
     }
 }
