c960fd5deb71fff2d74ab39d55a469af6096d09c
liangbo <liangbo@qiaxz.com>
2018-08-28 12:00:22 +0800

    砖块布局bug修复

--- code/viewstudy/src/main/java/codingbo/viewstudy/myLayout/BricksLayout.java
+++ code/viewstudy/src/main/java/codingbo/viewstudy/myLayout/BricksLayout.java
@@ -25,58 +25,26 @@ public class BricksLayout extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        int maxWidth = MeasureSpec.getSize(widthMeasureSpec) - getPaddingStart() - getPaddingEnd();
-        int maxheight = 0;
-
-        int rowHeight = 0;
-        int rowWidth = 0;
-
-        int layoutWidth = 0;
-        int layoutHeight = 0;
-
-        int cW;
-        int cH;
-
         int count = getChildCount();
         for (int i = 0; i < count; i++) {
-            View child = getChildAt(count);
-            measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
-            cW = child.getMeasuredWidth();
-            cH = child.getMeasuredHeight();
-
-            if (cW > maxWidth) {
-                // 单个 超过父View宽度
-                layoutWidth += rowWidth + cW;
-                layoutHeight += rowHeight + cH;
-
-                rowWidth = 0;
-                rowHeight = 0;
-            } else {
-                if (cW + layoutWidth > maxWidth) {
-                    // 多个 超过父view宽度
-                    layoutWidth += rowWidth;
-                    layoutHeight += rowHeight;
-
-                    rowWidth = cW;
-                    rowHeight = cH;
-                } else {
-                    // 多个 不超过父view宽度
-                    rowWidth += cW;
-                    rowHeight = cH > rowHeight ? cH : rowHeight;
-                }
+            View child = getChildAt(i);
+            if (child.getVisibility() == View.GONE) {
+                continue;
             }
-
-            setMeasuredDimension(resolveSize(layoutWidth, widthMeasureSpec),
-                    resolveSize(layoutHeight, heightMeasureSpec));
+            measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
         }
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
     }
 
 
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
     }
 
     @Override
@@ -99,42 +67,29 @@ public class BricksLayout extends ViewGroup {
         int count = getChildCount();
         for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
-
+            if (child.getVisibility() == View.GONE) {
+                continue;
+            }
             cW = child.getMeasuredWidth();
             cH = child.getMeasuredHeight();
 
-            if (cW > maxWidth) {
-                // 单个 超过父View宽度
-                // 另起一行
+            if (cW + usedWidth > maxWidth) {
+                // 单个或多个 超过父view宽度
+                usedHeight += maxRowHeight;
+
                 left = 0;
-                top = usedHeight + maxRowHeight;
+                top = usedHeight;
 
                 maxRowHeight = cH;
                 usedWidth = cW;
-                usedHeight += maxRowHeight;
-                maxRowHeight = 0;
 
             } else {
-                if (cW + usedWidth > maxWidth) {
-                    // 多个 超过父view宽度
+                // 多个 不超过父view宽度
+                left = usedWidth;
+                top = top;
 
-                    left = 0;
-                    top = usedHeight + maxRowHeight;
-
-                    maxRowHeight = cH;
-                    usedWidth = cW;
-                    usedHeight += maxRowHeight;
-                    maxRowHeight = 0;
-
-                } else {
-                    // 多个 不超过父view宽度
-                    left = usedWidth;
-                    top = top;
-
-                    maxRowHeight = maxRowHeight > cH ? maxRowHeight : cH;
-                    usedWidth += cW;
-                    
-                }
+                maxRowHeight = maxRowHeight > cH ? maxRowHeight : cH;
+                usedWidth += cW;
             }
 
             right = left + cW;
